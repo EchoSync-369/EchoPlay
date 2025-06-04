cb1c7634e297d4dac59955cd77648eaf99d7f2b2
Scheltema <thomas.scheltema@hp.com>
2025-06-04 11:58:38 -0500

    Home page fetches and logs generic content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent {
+  constructor(private http: HttpClient) {}
 
+  token: string = localStorage.getItem("access_token") || "";
+
+  ngOnInit() {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+    });
+
+    this.http
+      .get<any>("https://api.spotify.com/v1/browse/categories", { headers })
+      .subscribe({
+        next: (response) => {
+          console.log("Response received:", response);
+        },
+        error: (err) => {
+          console.error("Oopsies tehe:", err);
+        },
+      });
+  }
 }
