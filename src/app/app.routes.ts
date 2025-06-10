e92cc43f6f26c8ed099c259c862b58cf52befa95
Scheltema <thomas.scheltema@hp.com>
2025-06-10 16:16:14 -0500

    added fallback route for unmatched paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
-import { Routes } from '@angular/router';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { HomeComponent } from './pages/home/home.component';
-import { SpotifyAuthGuard } from './services/auth/spotify-auth.guard';
-import { UnauthGuard } from './services/unauth/unauth.guard';
-import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { PlayerPageComponent } from './pages/player-page/player-page.component';
+import { Routes } from "@angular/router";
+import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { SpotifyAuthGuard } from "./services/auth/spotify-auth.guard";
+import { UnauthGuard } from "./services/unauth/unauth.guard";
+import { SearchPageComponent } from "./pages/search-page/search-page.component";
+import { PlayerPageComponent } from "./pages/player-page/player-page.component";
 
 export const routes: Routes = [
   // Redirect to the landing page on base path
@@ -18,24 +18,29 @@ export const routes: Routes = [
   {
     path: "landing",
     component: LandingPageComponent,
-    canActivate: [UnauthGuard]
+    canActivate: [UnauthGuard],
   },
 
   // "Authenticated" routes
   {
     path: "home",
     component: HomeComponent,
-    canActivate: [SpotifyAuthGuard]
+    canActivate: [SpotifyAuthGuard],
   },
   {
     path: "search/:query",
     component: SearchPageComponent,
-    canActivate: [SpotifyAuthGuard]
+    canActivate: [SpotifyAuthGuard],
   },
   {
     path: "player/:type/:id",
     component: PlayerPageComponent,
-    canActivate: [SpotifyAuthGuard]
+    canActivate: [SpotifyAuthGuard],
   },
 
+  // Fallback route for any unmatched paths
+  {
+    path: "**",
+    redirectTo: "/landing",
+  },
 ];
